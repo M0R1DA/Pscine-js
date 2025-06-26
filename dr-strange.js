35b8b366c050040efa3ec272af8d60fc9f6e4b11
morida <morida@gmail.com>
2025-06-26 15:50:31 +0100

    data

---
+++ dr-strange.js
@@ -0,0 +1,25 @@
+function addWeek(data) {
+     const days= [
+        'Monday', 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday', 'Sunday'
+        ,'secondMonday', 'seconfTuesday' , 'secondWednesday','secondThursday' , 'secondFriday' ,  'secondSaturday' , 'secondSunday'
+     ]
+     const r = (data - new Date('0001-01-01'))/(1000*60*60*24) 
+     const rt = r%14
+    return days[rt]
+     
+}
+function timeTravel(obj) {
+   const dat = new Date(obj.date)
+   dat.setHours(obj.hour, obj.minute, obj.second)
+   return dat
+}
+console.log(addWeek(new Date('0001-01-11')))
+
+// console.log(
+
+// timeTravel({
+//   date: new Date('2020-05-29 23:25:22'),
+//   hour: 21,
+//   minute: 22,
+//   second: 22,
+// }).toString());
--- same-amount.js
+++ same-amount.js
@@ -1,11 +1,25 @@
-// const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
-//  ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
-function sameAmount(str, r1, r2) {
-    const ensureGlobal = r => new RegExp(r.source, r.flags.includes('g') ? r.flags : r.flags + 'g')
-    const m1 = str.match(ensureGlobal(r1)) || []
-    const m2 = str.match(ensureGlobal(r2)) || []
-    return m1.length === m2.length
+//  const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
+// //  ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
+// function sameAmount(str, r1, r2) {
+//     const ensureGlobal = r => new RegExp(r.source, r.flags.includes('g') ? r.flags : r.flags + 'g')
+//     const m1 = str.match(ensureGlobal(r1)) || []
+//     console.log(m1);
+    
+//     const m2 = str.match(ensureGlobal(r2)) || []
+//     console.log(m2);
+    
+//     return m1.length === m2.length
+// }
+
+// // console.log(sameAmount('hello how are you', /he/, /ho/));
+//  console.log(!sameAmount(data, /q /, /qqqqqqq/));
+function name(params) {
+    var rt= "j;gh;b2"
+}
+console.log(t);
+if (1){
+   var t= "yvqicb"
 }
 
-// console.log(sameAmount('hello how are you', /he/, /ho/));
-// console.log(!sameAmount(data, /q /, /qqqqqqq/));
+// !undefined === true
+//
